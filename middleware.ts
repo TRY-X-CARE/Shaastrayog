bd4e1a9ddcd5d4583f982ad3c598d05f64344d3d
TRY-X-CARE <tryx.care@gmail.com>
2025-07-02 22:08:18 +0530

    refactor: Simplify middleware function, update .gitignore, and add Upstash dependencies in package files

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,5 @@
-import { NextResponse } from 'next/server';
-import { Ratelimit } from '@upstash/ratelimit';
-import { Redis } from '@upstash/redis';
+import { NextResponse, NextRequest } from 'next/server';
 
-const redis = Redis.fromEnv();
-const ratelimit = new Ratelimit({ redis, limiter: Ratelimit.slidingWindow(100, '10 s') });
-
-export async function middleware(req) {
-  const ip = req.ip || req.headers.get('x-forwarded-for') || 'anonymous';
-  const { limit, reset, remaining } = await ratelimit.limit(ip);
-  if (!remaining) return new Response('Too many requests', { status: 429 });
+export function middleware(req: NextRequest) {
   return NextResponse.next();
 } 
