4b343d46c4b467268cad0c56bd7c759f98858551
TRY-X-CARE <tryx.care@gmail.com>
2025-07-03 17:16:52 +0530

    feat: Enhance Content Security Policy in Next.js config to support Razorpay integration and improve overall security headers

--- next.config.js
+++ next.config.js
@@ -1,11 +1,32 @@
 /** @type {import('next').NextConfig} */
 const securityHeaders = [
-  { key: 'Content-Security-Policy',
-    value: "default-src 'self'; img-src 'self' data: blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; object-src 'none'; frame-ancestors 'none'; base-uri 'self'; form-action 'self';" },
-  { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
-  { key: 'X-Content-Type-Options',  value: 'nosniff' },
-  { key: 'Referrer-Policy',        value: 'origin-when-cross-origin' },
-  { key: 'Permissions-Policy',     value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()' }
+  {
+    key: "Content-Security-Policy",
+    value: `
+      default-src 'self';
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://checkout.razorpay.com;
+      style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://checkout.razorpay.com;
+      font-src 'self' https://fonts.gstatic.com;
+      img-src 'self' data: blob: https://checkout.razorpay.com;
+      frame-src https://*.razorpay.com;
+      connect-src 'self' https://lumberjack.razorpay.com https://api.razorpay.com;
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+    `
+      .replace(/\s{2,}/g, " ")
+      .trim(),
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  { key: "X-Content-Type-Options", value: "nosniff" },
+  { key: "Referrer-Policy", value: "origin-when-cross-origin" },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+  },
 ];
 
 const nextConfig = {
@@ -16,7 +37,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: securityHeaders,
       },
     ];
